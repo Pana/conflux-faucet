25b03f5a852c62f15fe0a016e717d89af73252b4
PanaW <pan.wang@confluxnetwork.org>
2021-11-03 21:15:21 +0800

    update faucet address

--- frontend/src/contracts-config/faucetContractConfig.js
+++ frontend/src/contracts-config/faucetContractConfig.js
@@ -3,7 +3,7 @@ import { abi } from "../../../build/contracts/Faucet.json";
 // 水龙头合约地址
 // 要求为 CIP-37 格式的测试网地址
 // 1000 cfx each claim, 3600s interval
-const faucetAddress = "NET8888:TYPE.CONTRACT:ACG2V19UETGZ4NCNUZFD09XS1HWZ2B66CYMVEZ8ZUS";
+const faucetAddress = "NET8888:TYPE.CONTRACT:ACDKDB5RYVBV7VKUU9JEWRAFJ2HAHF2YHJVX118BMA";
 
 /** 
  * faucetContractConfig[netId] = { abi, address }
